783dba10357c6c608bffc8ac1a5e0bcde0d08652
thành <tienthanh2171998@gmail.com>
2019-09-28 21:44:12 +0700

    Demo load ảnh

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,7 @@
 import * as WebBrowser from 'expo-web-browser';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { Button } from '@ant-design/react-native'
 import React from 'react';
 import {
   Image,
@@ -9,62 +12,14 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-
+import styles from '../Css/HomeScreen'
 import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
+      <Home />
     </View>
   );
 }
@@ -73,126 +28,100 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isReady: false,
+      listNovel: [
+        {
+          Id: 1, Name: 'Yêu thần kí',
+          View: 10, Hot: true,
+          ImgUrl: 'https://tienthanh217.000webhostapp.com/AppTruyen/yeu-than-ky.jpg',
+          Type: 'Truyện kiếm hiệp',
+          Author: 'Tiến Tiến',
+          DateModify: '1/1/2001',
+          Continue: false,
+          chapNumber: 10
+        },
+        {
+          Id: 2, Name: 'Tiếu ngạo',
+          View: 10, Hot: true,
+          ImgUrl: 'https://tienthanh217.000webhostapp.com/AppTruyen/yeu-than-ky.jpg',
+          Type: 'Truyện kiếm hiệp',
+          Author: 'Tiến Tiến',
+          DateModify: '1/1/2001',
+          Continue: false,
+          chapNumber: 10
+        },
+      ],// Ví dụ về 1 sách
+    }
+  }
+  async componentDidMount() {
+    await Font.loadAsync(
+      'antoutline',
+      // eslint-disable-next-line
+      require('../node_modules/@ant-design/icons-react-native/fonts/antoutline.ttf')
     );
 
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
+    await Font.loadAsync(
+      'antfill',
+      // eslint-disable-next-line
+      require('../node_modules/@ant-design/icons-react-native/fonts/antfill.ttf')
     );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
+    // eslint-disable-next-line
+    this.setState({ isReady: true });
+  }
+  render() {
+    const data = this.state ? this.state.listNovel : [];
+    return (<>
+
+      <View style={styles.welcomeContainer}>
+        <Image
+          source={
+            __DEV__
+              ? require('../assets/images/robot-dev.png')
+              : require('../assets/images/robot-prod.png')
+          }
+          style={styles.welcomeImage}
+        />
+      </View>
+      <View>
+        <Button style={{ width: 120 }}> Hêl9 </Button>
+      </View>
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{ uri: 'https://tienthanh217.000webhostapp.com/AppTruyen/yeu-than-ky.jpg' }}></Image>
+
+      <View>
+        <Text>
+          {data.length}
+        </Text>
+      </View>
+
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+        <View>
+          {data.map((item) =>
+            <View>
+              <Text>
+                {item.Name}
+              </Text>
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{ uri: item.ImgUrl}}></Image>
+            </View>)}
+        </View>
+      </ScrollView>
+
+
+    </>);
   }
 }
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
 
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+
+
+
