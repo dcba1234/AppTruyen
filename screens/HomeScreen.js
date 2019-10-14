fe1720fd3a2f992ac0a6aa2ab74d475ec678d35b
Cuong <cuongdao1298@gmail.com>
2019-10-14 22:44:17 +0700

    them data

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -21,10 +21,6 @@ import { MonoText } from "../components/StyledText";
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-      
-=======
->>>>>>> mobile
       <Home />
     </View>
   );
@@ -39,45 +35,9 @@ class Home extends React.Component {
     super(props);
     this.state = {
       isReady: false,
-<<<<<<< HEAD
-      listNovel: [
-        {
-          Id: 1, Name: 'Yêu thần kí',
-          View: 10, Hot: true,
-          ImgUrl: 'https://tienthanh217.000webhostapp.com/AppTruyen/yeu-than-ky.jpg',
-          Type: 'Truyện kiếm hiệp',
-          Author: 'Tiến Tiến',
-          DateModify: '1/1/2001',
-          Continue: false,
-          chapNumber: 10
-        },
-        {
-          Id: 2, Name: 'Tiếu ngạo',
-          View: 10, Hot: true,
-          ImgUrl: 'https://tienthanh217.000webhostapp.com/AppTruyen/yeu-than-ky.jpg',
-          Type: 'Truyện kiếm hiệp',
-          Author: 'Tiến Tiến',
-          DateModify: '1/1/2001',
-          Continue: false,
-          chapNumber: 10
-        },
-        {
-          Id: 3, Name: 'Võ lâm truyền kỳ',
-          View: 10, Hot: false,
-          ImgUrl: 'https://tienthanh217.000webhostapp.com/AppTruyen/yeu-than-ky.jpg',
-          Type: 'Truyện kiếm hiệp',
-          Author: 'Tiến Tiến',
-          DateModify: '1/1/2001',
-          Continue: true,
-          chapNumber: 10
-        },
-      ],// Ví dụ về 1 sách
-    }
-=======
       listNovel: [] // cái này xem trong log để xem kiểu dữ liệu
     };
 
->>>>>>> mobile
   }
 
   async componentDidMount() {
@@ -89,63 +49,10 @@ class Home extends React.Component {
       "antfill",
       require("../node_modules/@ant-design/icons-react-native/fonts/antfill.ttf")
     );
-<<<<<<< HEAD
-    // eslint-disable-next-line
-    this.setState({ isReady: true });
-  }
-  render() {
-    const data = this.state ? this.state.listNovel : [];
-    return (<>
-      {/* <Slideshow 
-      dataSource={[
-        { url:'https://tienthanh217.000webhostapp.com/AppTruyen/yeu-than-ky.jpg' },
-        { url:'https://tienthanh217.000webhostapp.com/AppTruyen/yeu-than-ky.jpg' },
-        { url:'https://tienthanh217.000webhostapp.com/AppTruyen/yeu-than-ky.jpg' }
-    ]}/> */}
-      <View style={styles.welcomeContainer}>
-        <Image
-          source={
-            __DEV__
-              ? require('../assets/images/robot-dev.png')
-              : require('../assets/images/robot-prod.png')
-          }
-          style={styles.welcomeImage}
-        />
-      </View>
-    
-      <Image
-        style={{ width: 450, height: 200 }}
-        source={{ uri: 'http://www.nhahocotton.org.vn/wp-content/uploads/2018/04/thu-kiem-truong-an.jpg'}}>
-        
-      </Image>
-
-      <View>
-        {/* <Text>
-          {data.length}
-        </Text> */}
-      </View>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View>
-          {data.map((item) =>    // cái này chắc k cần gt nhể =>>
-            <View style={{flexDirection: "row"}}>
-              
-              <Image
-                style={{ width: 200, height: 200, margin: 5 }}
-                source={{ uri: item.ImgUrl}}></Image>
-                <Text>
-                {item.Name}
-              </Text>
-              
-            </View>)}
-        </View>
-      </ScrollView>
-=======
 
     let list = await NovelService.getAll();
     this.setState({ isReady: true, listNovel: list });
-    console.log(list);
+    console.log(list); 
   }
 
   reLoad = async() => {
@@ -153,7 +60,6 @@ class Home extends React.Component {
     let list = await NovelService.getAll();
     this.setState({isReady:true,listNovel: list})
   }
->>>>>>> mobile
 
   render() {
 
@@ -168,7 +74,7 @@ class Home extends React.Component {
           }
         >
           <View>
-            {data.map((
+            {data.map((  
               item,
               index // cái này chắc k cần gt nhể =>>
             ) => (
